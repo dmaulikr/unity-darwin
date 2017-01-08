3173e751be8644d8fa51e1e26543df3388a93c42
mikestratton <mike@mikestratton.net>
2017-01-08 00:52:28 -0500

    reverted to working commit

---
+++ dev/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+    Transform prey;               // Reference to the player's position.
+    public string preyTag;
+    //PlayerHealth playerHealth;      // Reference to the player's health.
+   // EnemyHealth enemyHealth;        // Reference to this enemy's health.
+    NavMeshAgent nav;               // Reference to the nav mesh agent.
+    //Vector3 gameObjectPosition; location to test for out of bounds.
+
+    void Awake()
+    {
+        // Set up the references.
+        prey = GameObject.FindGameObjectWithTag(preyTag).transform;
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+
+    void Update()
+    {
+
+            // ... set the destination of the nav mesh agent to the player.
+            if (prey != null)
+            nav.SetDestination(prey.position);
+
+            // destroy gameobject if out of bounds
+           // if (GameObject.t)
+
+    }
+}
